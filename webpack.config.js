4c00365b4858dd46dd5b67a7da598d1901b9152e
whisky87 <vanbinh87@gmail.com>
2018-08-08 17:42:45 +0700

    Fixed: program configurations

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackCopyPlugin = require('copy-webpack-plugin');
+var plugins = [];
+plugins.push(new WebpackShellPlugin({
+    onBuildStart: ['echo "Starting build by webpack"'],
+    onBuildEnd: ['node ./dist/bundle.js']
+}));
+// plugins.push(new WebpackCopyPlugin([
+//     { from: 'lib/data', to: 'data' }
+// ]));
+module.exports = {
+    entry: './server/server.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    target: 'node',
+    watch: false,
+    plugins: plugins,
+    node: {
+        __dirname: false, //giữ nguyên the regular Node.js __dirname behavior
+        __filename: false
+    }
+}
