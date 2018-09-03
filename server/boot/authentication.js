97a7d6682143a8d5519219928bb5f3364797b8fd
whisky87 <vanbinh87@gmail.com>
2018-09-03 14:29:42 +0700

    Fixed: disable 'always clean up token each time app restarts'

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -7,36 +7,41 @@ module.exports = function enableAuthentication(server) {
   //create user
   var { user, Role, RoleMapping } = server.models;
 
-  user.upsertWithWhere(
-    { email: 'admin@coachingcloud.com' },
-    { email: 'admin@coachingcloud.com', password: 'admin@123' },
-    function(err, user) {
-      if (err) {
-        return helper.inspect(err);
-      }
+  user.find({ where: { email: 'admin@coachingcloud.com' } }, (err, user) => {
+    if (user == null) {
+      //not exists
 
-      if (user) {
-        Role.upsertWithWhere({ name: 'admin' }, { name: 'admin' }, function(
-          err,
-          role
-        ) {
+      user.create(
+        { email: 'admin@coachingcloud.com', password: 'admin@123' },
+        function(err, user) {
           if (err) {
             return helper.inspect(err);
           }
 
-          if (role) {
-            role.principals.create(
-              {
-                principalType: RoleMapping.USER,
-                principalId: user.id
-              },
-              function(err, principal) {
-                if (err) return helper.inspect(err);
+          if (user && user.isNewRecord()) {
+            Role.upsertWithWhere({ name: 'admin' }, { name: 'admin' }, function(
+              err,
+              role
+            ) {
+              if (err) {
+                return helper.inspect(err);
               }
-            );
+
+              if (role) {
+                role.principals.create(
+                  {
+                    principalType: RoleMapping.USER,
+                    principalId: user.id
+                  },
+                  function(err, principal) {
+                    if (err) return helper.inspect(err);
+                  }
+                );
+              }
+            });
           }
-        });
-      }
+        }
+      );
     }
-  );
+  });
 };
