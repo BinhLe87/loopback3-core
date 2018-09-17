b97d9ab0fb0f8f15ab44d1c113da55afa27a7c54
whisky87 <vanbinh87@gmail.com>
2018-09-17 17:07:20 +0700

    feat($server): automatic sharing workbook
    Added: automatic sharing workbook once a new workbook is created by a logged-in user and matching all sharing conditions
    
    Relates #85

--- server/api/database/models/mysql/workbook.js
+++ server/api/database/models/mysql/workbook.js
@@ -1,8 +1,24 @@
 'use strict';
 const app = require('../../../server');
 const Promise = require('bluebird');
+const WorkbookUtil = require('./workbook.util');
 
 module.exports = function(Workbook) {
+  Workbook.observe('after save', async function(ctx) {
+    var instance = ctx.instance || ctx.currentInstance;
+    var created_workbook_id = instance.id;
+
+    //trigger autoshare workbook by current logged in user
+    const token = ctx.options && ctx.options.accessToken;
+    const userId = token && token.userId;
+
+    if (userId) {
+      //already logged-in
+
+      WorkbookUtil.autoShareWorkBook(userId, created_workbook_id);
+    }
+  });
+
   //Delete this workbook from any place has relation to, may be from libary or program, etc.
   Workbook.observe('after delete', async function(ctx) {
     var deleted_workbook_id = ctx.where.id;
--- server/api/database/models/mysql/workbook.util.js
+++ server/api/database/models/mysql/workbook.util.js
@@ -1,6 +1,9 @@
 'use strict';
 const app = require('../../../server');
 const Promise = require('bluebird');
+const UserSettingUtil = require('./user-setting.util');
+const UserUtil = require('./user.util');
+const debug = require('debug')('workbook.util.js');
 
 module.exports = exports = {};
 
@@ -21,4 +24,45 @@ async function getWorkbookById(workbookId) {
   return found_workbok;
 }
 
+async function autoShareWorkBook(sharing_user_id, workbook_id) {
+  if (!sharing_user_id || !workbook_id) {
+    return;
+  }
+
+  /**
+   *  return list of user_ids will be shared workbook by sharing user_id
+   *
+   * @param {*} sharing_user_id
+   * @returns {array} return an array of user_ids if found.
+   * Otherwise, return empty array if the auto_share setting is unavailable due to either inactive or not array type.
+   */
+  async function __getSharedUserIds(sharing_user_id) {
+    const AUTO_SHARE_SETTING_CODE = 'auto_share_workbook';
+    var user_setting = await UserSettingUtil.getUserSettingBySettingCode(
+      sharing_user_id,
+      AUTO_SHARE_SETTING_CODE
+    );
+
+    var sharedUserIds = _.get(user_setting, 'value', null);
+
+    if (!Array.isArray(sharedUserIds)) {
+      //either null value or not array type
+      return [];
+    }
+
+    return sharedUserIds;
+  }
+
+  var sharedUserIds = await __getSharedUserIds(sharing_user_id);
+  for (let sharedUserId of sharedUserIds) {
+    var created_workbookshare = await UserUtil.shareWorkBook(
+      sharing_user_id,
+      sharedUserId,
+      workbook_id
+    );
+    debug(created_workbookshare);
+  }
+}
+
 exports.getWorkbookById = getWorkbookById;
+exports.autoShareWorkBook = autoShareWorkBook;
