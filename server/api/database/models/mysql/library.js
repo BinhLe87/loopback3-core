09186f87bfcf426ece91206feff43eafba2fbd9a
whisky87 <vanbinh87@gmail.com>
2018-09-29 10:49:07 +0700

    fix($server): convert image file name to image url in Library.afterRemote('*.__get__workbooks')
    convert image file name to image url in Library.afterRemote('*.__get__workbooks')

--- server/api/database/models/mysql/library.js
+++ server/api/database/models/mysql/library.js
@@ -1,3 +1,32 @@
 'use strict';
+const loopback_util = require('../../../helpers/loopbackUtil');
 
-module.exports = function(Library) {};
+module.exports = function(Library) {
+  /**
+   * - Transform image file name to image url
+   */
+  Library.afterRemote('*.__get__workbooks', function(ctx, modelInstance, next) {
+    var ctx_result = ctx.result;
+
+    if (_.isEmpty(ctx_result)) return next();
+
+    var item_array = Array.isArray(ctx_result) ? ctx_result : [ctx_result];
+
+    for (let item_ele of item_array) {
+      var image_url = _.get(item_ele, 'image_url');
+
+      if (image_url) {
+        var transformed_file_name = image_url;
+        var transformed_file_url = loopback_util.convertTransformedFileNameToFileURL(
+          ctx,
+          transformed_file_name
+        );
+
+        //update new image url back to ctx.result
+        item_ele.image_url = transformed_file_url;
+      }
+    }
+
+    next();
+  });
+};
