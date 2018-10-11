50915c2f40efb021488bbcdcb0d973a2d57e926d
whisky87 <vanbinh87@gmail.com>
2018-10-11 12:41:24 +0700

    debug($server): add script print out to debug
    add script print out to debug

--- script/install_npm_modules.js
+++ script/install_npm_modules.js
@@ -12,3 +12,7 @@ command = 'npm list loopback-datasource-juggler';
 console.log(command);
 shell.exec(command, { async: true });
 
+///HACK: Print content file for debug
+command = `sed -n '410 ,430p' node_modules/loopback/node_modules/loopback-datasource-juggler/lib/include.js`;
+console.log(command);
+shell.exec(command, { async: true });
