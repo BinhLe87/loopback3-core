7d115326ab0fc4c5f752acafe2efc7d9d6948480
whisky87 <vanbinh87@gmail.com>
2018-10-17 09:30:38 +0700

    feat($server): add constraint of unique display position for chapters within one workbook
    add constraint of unique display position for chapters within one workbook

--- server/api/boot/defaultScopeAllModels.js
+++ server/api/boot/defaultScopeAllModels.js
@@ -2,16 +2,14 @@
 const debug = require('debug')(`${__filename}`);
 
 module.exports = function(app) {
-  var models = app._models;
-
-  for (let model of models) {
-    if (model.name == 'page') {
-      model.defaultScope = function(target, inst) {
-        var scope = this.definition.settings.scope;
-        Object.assign(scope, { limit: 2 });
-
-        return scope;
-      };
-    }
-  }
+  // var models = app._models;
+  // for (let model of models) {
+  //   if (model.name == 'page') {
+  //     model.defaultScope = function(target, inst) {
+  //       var scope = this.definition.settings.scope;
+  //       Object.assign(scope, { limit: 2 });
+  //       return scope;
+  //     };
+  //   }
+  // }
 };
