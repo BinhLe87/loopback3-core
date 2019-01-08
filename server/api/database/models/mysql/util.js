146e3da57c626971d6e4657d1db4eb1f09abf0bc
whisky87 <vanbinh87@gmail.com>
2019-01-08 16:34:09 +0700

    fix($server): combine all functions about uploading file into one file controllers/upload/uploadFile.js and fix multer issue
    - Edited: combine all functions about uploading file into one file controllers/upload/uploadFile.js
    - Fixed: accept any file name field in multipart form data by replacing multer.single() with multer.any(). Uploaded files will be stored in req.files variable
    
    Relates: #138

--- server/api/database/models/mysql/util.js
+++ server/api/database/models/mysql/util.js
@@ -3,7 +3,7 @@
 const debug = require('debug')('util.js');
 const {
   uploadFileController
-} = require('../../../controllers/upload/uploadFileController');
+} = require('../../../controllers/upload/uploadFile');
 
 module.exports = async function(Util) {
   Util.upload = async function(ctx, options, cb) {
