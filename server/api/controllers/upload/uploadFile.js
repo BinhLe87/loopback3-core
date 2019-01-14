a886d86f07ba65fd3d0d09b86e0b9e39059b7fb0
whisky87 <vanbinh87@gmail.com>
2019-01-14 12:59:03 +0700

    fix($server): receive uploaded filename via req.files instead of using global variable in callback function
    Fixed: receive uploaded filename via req.files instead of using global variable in callback function because it leads wrong 'file path' value in case of uploading multiple files at the same time

--- server/api/controllers/upload/uploadFile.js
+++ server/api/controllers/upload/uploadFile.js
@@ -19,21 +19,26 @@ const baseJoiOptions = {
 };
 
 var uploadQueryParamsJoi = Joi.alternatives().try(
+  Joi.alternatives().try(
+    Joi.object()
+    .keys({
+      file_type: Joi.string()
+        .valid('workbook_image')
+        .required(),
+      workbook_id: Joi.string().required()
+    }).unknown()
 
-  Joi.object()
-  .keys({
-    file_type: Joi.string()
-      .valid('workbook_image')
-      .required(),
-    workbook_id: Joi.string().required()
-  })
+  )
 );
 
 async function uploadFileController(ctx) {
 
   try {
 
-    var {req, res} = ctx;
+    var {
+      req,
+      res
+    } = ctx;
     var query_params_origin = req.query;
 
     var url_params_joi_result = uploadQueryParamsJoi.validate(query_params_origin, baseJoiOptions);
@@ -71,11 +76,8 @@ async function uploadFileController(ctx) {
 async function saveFile(req, res, options) {
   //multer.bind(app);
 
-  var relativeFilePathWillSave;
-  var absoluteFilePathWillSave;
-
   var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
       var origin_filename = file.originalname;
       var fileNameWillSave = uploadFilePathHandler.transformFileNameToSave(
         origin_filename
@@ -86,25 +88,16 @@ async function saveFile(req, res, options) {
       );
       cb(null, pathWillSave);
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
       var origin_filename = file.originalname;
       var fileNameWillSave = uploadFilePathHandler.transformFileNameToSave(
         origin_filename
       );
       cb(null, fileNameWillSave);
-
-      absoluteFilePathWillSave = path.join(
-        uploadFilePathHandler.identifyAbsoluteDirPathWillSave(fileNameWillSave),
-        fileNameWillSave
-      );
-      relativeFilePathWillSave = path.join(
-        uploadFilePathHandler.identifyRelativeDirPathWillSave(fileNameWillSave),
-        fileNameWillSave
-      );
     }
   });
 
-  var fileFilter = function(req, file, cb) {
+  var fileFilter = function (req, file, cb) {
     //cb(new Error('Not allowed to upload at the moment!!!!'));
     cb(null, true);
   };
@@ -121,11 +114,6 @@ async function saveFile(req, res, options) {
   var my_uploadPromise = Promise.promisify(my_upload).bind(my_upload);
 
   await my_uploadPromise(req, res);
-
-  return {
-    relativeFilePathWillSave: relativeFilePathWillSave,
-    absoluteFilePathWillSave: absoluteFilePathWillSave
-  };
 };
 
 
@@ -138,13 +126,13 @@ async function _routeUploadControllerByFileType(ctx, query_params) {
   switch (file_type) {
 
     case 'workbook_image':
-        result = await _workbook_image_uploader(ctx, query_params);        
-        resultType = 'workbook';
-        break;
-    default:        
+      result = await _workbook_image_uploader(ctx, query_params);
+      resultType = 'workbook';
+      break;
+    default:
   }
 
-  if(_.isEmpty(result) || _.isEmpty(resultType)) {
+  if (_.isEmpty(result) || _.isEmpty(resultType)) {
 
     throw new Error('result and resultType must be not empty');
   }
@@ -161,7 +149,10 @@ async function _routeUploadControllerByFileType(ctx, query_params) {
  * @param {object} query_params query parameters
  * @returns {object} workbook object after updating
  */
-async function _workbook_image_uploader({req, res}, query_params) {
+async function _workbook_image_uploader({
+  req,
+  res
+}, query_params) {
 
   var WorkbookModel = app.models.workbook;
   var findByIdPromise = Promise.promisify(WorkbookModel.findById).bind(
@@ -176,18 +167,41 @@ async function _workbook_image_uploader({req, res}, query_params) {
     throw Boom.notFound(`Not found workbook_id is ${workbook_id}`);
   }
 
-var {
-  relativeFilePathWillSave,
-  absoluteFilePathWillSave
-} = await saveFile(
-  req, res
-);
+    await saveFile(
+      req, res
+    );
 
-var standardized_file_name = path.basename(relativeFilePathWillSave);
-var updateAttributeWorkbookPromise = Promise.promisify(workbook_found.updateAttribute).bind(workbook_found);
-var updateAttribute_result = await updateAttributeWorkbookPromise('image_url', standardized_file_name);
+    var uploaded_files = req.file || req.files;
+    uploaded_files = _.castArray(uploaded_files);
 
-return updateAttribute_result;
+    if (_.isEmpty(uploaded_files)) {
+
+      throw Boom.notFound('Not found any files need to be uploaded');
+    }
+
+    var relativeFilePathWillSave;
+    var absoluteFilePathWillSave;  
+    var updateAttribute_result;
+    for (const [index, file] of uploaded_files.entries()) {
+
+      var fileNameWillSave = file.filename;
+
+      absoluteFilePathWillSave = path.join(
+        uploadFilePathHandler.identifyAbsoluteDirPathWillSave(fileNameWillSave),
+        fileNameWillSave
+      );
+      relativeFilePathWillSave = path.join(
+        uploadFilePathHandler.identifyRelativeDirPathWillSave(fileNameWillSave),
+        fileNameWillSave
+      );
+
+      var standardized_file_name = path.basename(relativeFilePathWillSave);
+      var updateAttributeWorkbookPromise = Promise.promisify(workbook_found.updateAttribute).bind(workbook_found);
+      updateAttribute_result = await updateAttributeWorkbookPromise('image_url', standardized_file_name);
+    }
+  
+    
+  
+    return updateAttribute_result;
+ 
 }
-
-
