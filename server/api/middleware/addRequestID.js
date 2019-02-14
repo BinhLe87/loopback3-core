fe66e65ddb0512e059ce681a1b98a25bf80dd133
whisky87 <vanbinh87@gmail.com>
2019-02-14 09:23:06 +0700

    feat($server): add X-Request-ID in HTTP header
    - Added: add X-Request-ID in HTTP header
    
    Relates: #154

---
+++ server/api/middleware/addRequestID.js
@@ -0,0 +1,15 @@
+const uuidv4 = require('uuid/v4');
+
+//Add X-Request-ID in HTTP header
+module.exports = function() {
+  return function addRequestID(req, res, next) {
+    //check whether exists X-Request-ID in HTTP header
+    var request_id = req.headers['X-Request-ID'];
+    request_id = request_id ? request_id : uuidv4();
+
+    req.headers['X-Request-ID'] = request_id;
+    res.setHeader('X-Request-ID', request_id);
+
+    next();
+  };
+};
