c9bcd4b63efd4d5df606b367415caf649238745a
whisky87 <vanbinh87@gmail.com>
2019-03-07 10:25:14 +0700

    feat($server): edit script to build docker file
    edit script to build docker file

--- script/build_run_docker.js
+++ script/build_run_docker.js
@@ -5,7 +5,10 @@ const program = require('commander');
 program
   .version('0.1.0')
   .option('-e, --env <node_env>', 'NODE_ENVIRONMENT')
+  .option('-t, --target <docker_stage>', 'DOCKER_STAGE')
   .parse(process.argv);
+const DOCKER_PARAMS_NAME = ['target'];  //Declare docker params will be used
+
 
 program.parse(process.argv);
 
@@ -29,6 +32,23 @@ const image_tag = crypto.randomFillSync(buf).toString('hex');
 shell.pushd(`${__dirname}/./..`);
 
 console.log(`Creating image ${image_name}:${image_tag}`);
-exec(`docker build -t ${image_name}:${image_tag} -f Dockerfile.apc-master.${program.env} .`);
 
-exec(`docker images | grep ${image_tag} | awk '{print $3}' | xargs -I {} docker run -it -d --name ${image_name}-${image_tag} -v /Users:/Users -p 49173:8080 -p 15672:15672 -p 5672:5672 -p 6379:6379 {} tail -f /dev/null`);
+//Docker build params
+var docker_params = [];
+for (let docker_param_name of DOCKER_PARAMS_NAME) {
+
+  if(program[docker_param_name]) {
+
+    docker_params.push(`--${docker_param_name} ${program[docker_param_name]}`);
+  }
+}
+
+exec_wrapper(`docker build ${docker_params.join(' ')} -t ${image_name}:${image_tag} -f Dockerfile.apc-master.${program.env} .`);
+
+exec_wrapper(`docker images | grep ${image_tag} | awk '{print $3}' | xargs -I {} docker run -it -d --name ${image_name}-${image_tag} -v /Users:/Users -p 49173:8080 -p 15672:15672 -p 5672:5672 -p 6379:6379 -p 5044:5044 {} tail -f /dev/null`);
+
+function exec_wrapper(command) {
+
+  console.log(`Will run: ${command}`);
+  exec(command);
+}
