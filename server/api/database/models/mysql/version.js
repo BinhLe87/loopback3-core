9f89d5a3eebe9f9e101b900076b951907969c735
Dat Pham <duydatyds@gmail.com>
2019-05-07 23:49:02 +1200

    fix version content display

--- server/api/database/models/mysql/version.js
+++ server/api/database/models/mysql/version.js
@@ -4,8 +4,9 @@ module.exports = function(Version) {
   Version.observe('loaded', async function(ctx) {
     // check if version details
     var params = ctx.options.req.params;
+    var route = ctx.options.req.route;
 
-    if (params.fk) {
+    if (['/:id', '/:id/versions/:fk'].indexOf(route.path) >= 0) {
       // do nothing
     } else {
       ctx.data.content = {};
