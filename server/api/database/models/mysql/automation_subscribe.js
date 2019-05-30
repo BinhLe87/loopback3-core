75e06bc3ddf000f559bdf798b2b119b6a38526f8
Binh Le <vanbinh87@gmail.com>
2019-05-30 16:47:58 +0700

    feat($server): add 'automation_subscribe' model
    add 'automation_subscribe' model

---
+++ server/api/database/models/mysql/automation_subscribe.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const debug = require('debug')('util.js');
+const app = require('../../../server');
+const mysql_db = app.dataSources.cc_mysql;
+const Promise = require('bluebird');
+
+module.exports = async function(AutomationSubscribe) {
+  
+};
--- server/api/database/models/mysql/version.js
+++ server/api/database/models/mysql/version.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = function(Version) {
-  Version.observe('loaded', async function(ctx) {
-    // check if version details
-    var params = ctx.options.req.params;
-    var route = ctx.options.req.route;
+  // Version.observe('loaded', async function(ctx) {
+  //   // check if version details
+  //   var params = ctx.options.req.params;
+  //   var route = ctx.options.req.route;
 
-    if (['/:id', '/:id/versions/:fk'].indexOf(route.path) >= 0) {
-      // do nothing
-    } else {
-      ctx.data.content = {};
-    }
+  //   if (['/:id', '/:id/versions/:fk'].indexOf(route.path) >= 0) {
+  //     // do nothing
+  //   } else {
+  //     ctx.data.content = {};
+  //   }
 
-    return;
-  });
+  //   return;
+  // });
 };
