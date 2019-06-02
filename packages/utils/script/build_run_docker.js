c23fffc580ff3174d67c7656ccde24b6c4c5cfff
Binh Le <vanbinh87@gmail.com>
2019-06-03 00:42:41 +0700

    refactor: upgrade lib versions

--- packages/utils/script/build_run_docker.js
+++ packages/utils/script/build_run_docker.js
@@ -1,21 +1,30 @@
 const shell = require("shelljs");
 const crypto = require("crypto");
 const program = require("commander");
+const path = require("path");
 
 module.exports = exports = build_run_docker();
 
 function build_run_docker() {
   program
     .version("0.1.0")
-    .option("-e, --env <node_env>", "NODE_ENVIRONMENT")
+    .option("-i, --imagename <image_name>", "IMAGE_NAME")
     .option("-t, --target <docker_stage>", "DOCKER_STAGE")
+    .option("-f, --dockerfile <docker_file>. Default is Dockerfile", "DOCKER_FILE")
+    .option("-r, --home_root <home_root>", "HOME_ROOT")
+
     .parse(process.argv);
   const DOCKER_PARAMS_NAME = ["target"]; //Declare docker params will be used
 
   program.parse(process.argv);
 
-  if (typeof program.env === "undefined") {
-    console.error("Must identify NODE_ENV!!!");
+  if (typeof program.home_root === "undefined") {
+    console.error("Must identify --home_root!!!");
+    process.exit(1);
+  }
+
+  if (typeof program.imagename === "undefined") {
+    console.error("Must identify --imagename!!!");
     process.exit(1);
   }
 
@@ -25,11 +34,11 @@ function build_run_docker() {
     shell.exec(...arguments);
   }
 
-  const image_name = "apc-api";
+  const image_name = program.imagename;
   const buf = Buffer.alloc(3); //create random 6 characters
   const image_tag = crypto.randomFillSync(buf).toString("hex");
 
-  shell.pushd(`${__dirname}/./..`);
+  shell.pushd(`${program.home_root}`);
 
   console.log(`Creating image ${image_name}:${image_tag}`);
 
@@ -43,14 +52,16 @@ function build_run_docker() {
     }
   }
 
+  var dockerfile_name = program.dockerfile || 'Dockerfile';
+  var dockerfile_path = path.resolve(program.home_root, dockerfile_name);
   exec_wrapper(
     `docker build ${docker_params.join(
       " "
-    )} -t ${image_name}:${image_tag} -f Dockerfile.apc-master.${program.env} .`
+    )} -t ${image_name}:${image_tag} -f ${dockerfile_path} .`
   );
 
   exec_wrapper(
-    `docker images | grep ${image_tag} | awk '{print $3}' | xargs -I {} docker run -it -d --name ${image_name}-${image_tag} -v /Users:/Users -p 49173:8080 -p 15672:15672 -p 5672:5672 -p 6379:6379 -p 5044:5044 {} tail -f /dev/null`
+    `docker images | grep ${image_tag} | awk '{print $3}' | xargs -I {} docker run -it -d --name ${image_name}-${image_tag} -v /Users:/Users -p 49173:8080 -p 15672:15672 -p 5672:5672 -p 6379:6379 -p 5044:5044 -p 3001:3000 {} tail -f /dev/null`
   );
 
   function exec_wrapper(command) {
