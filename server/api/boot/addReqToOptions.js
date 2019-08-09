aecc485db10e8cb53bd2aad932823caa6fa1e6e5
Binh Le <vanbinh87@gmail.com>
2019-08-09 14:14:20 +0700

    fix: issue `unable to determine userId` in addReqToOptions mixin

--- server/api/boot/addReqToOptions.js
+++ server/api/boot/addReqToOptions.js
@@ -6,10 +6,13 @@ module.exports = function(app) {
     .phases.addBefore('invoke', 'add-request-to-options')
     .use(function(ctx, next) {
 
-      var current_user_id = ctx.args.options.accessToken.userId;
+      var current_user_id = _.get(ctx, 'args.options.accessToken.userId');
 
       _.set(ctx, 'args.options.req', ctx.req);
-      _.set(ctx, 'args.options.current_user_id', current_user_id);
+      if (current_user_id) {
+
+        _.set(ctx, 'args.options.current_user_id', current_user_id);
+      }
 
       //set user_id back to req
       // var req = ctx.req;
